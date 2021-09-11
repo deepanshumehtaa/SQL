17650c33d0cc698d301cf228594d3cced1606cff
Deepanshu mehta <40016257+deepanshumehtaa@users.noreply.github.com>
2021-09-12 02:41:15 +0530

    Index are created

--- Basics/Indexing_2.cpp
+++ Basics/Indexing_2.cpp
@@ -1,10 +1,20 @@
-invisible index in MySQL::
+This file covers:
+1. Invisible Index
+2. Clustered Index
+3. MySQL Prefix Index
+4. Removing Index
+
+and Advanced Topics like:
+USE INDEX hint –
+FORCE INDEX hint 
+
+invisible index::>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
 The invisible indexes allow you to mark indexes as unavailable for the query optimizer. 
 MySQL maintains the invisible indexes and keeps them up to date when the data in the columns associated with the indexes changes (SUPEREB).
 
-By default, indexes are visible.
-  
+> By default, indexes are visible.
+>   The index on the primary key column cannot be invisible  
 # Eg1
 
 CREATE INDEX index_name
@@ -18,14 +28,23 @@ ALTER INDEX index_name VISIBLE;
 "INFORMATION_SCHEMA" provides access to database metadata, information about the MySQL server such as 
 name of a database or table, the data type of a column, or access privileges. 
   
+In Django::...................................
+
+class Customer(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+
+    class Meta:
+        indexes = [
+            models.Index(fields=['last_name', 'first_name'], name='full_name_idx'),
+            models.Index(fields=['first_name'], name='first_name_idx'),
+        ]
+ 
+Clustered index::>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+A clustered index is a special type of index that reorders the way records in the table are physically stored.
+  
+This is Reallyt Expensive and Complex better to avoid
   
   
-  
-  
-  
-  
-  
-  
-  
-  
+MySQL Prefix Index::>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
   
